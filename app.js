2d68a82279bdb71fbb9db74c3120c0db2804d4f4
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-06-19 00:47:16 -0400

    avances de js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 const inputText = document.querySelector("input")
+const contenedorimg = document.getElementById(Element)
+
+inputText.addEventListener("keypress", (event) => {
+    let key = event.which || event.keyCode;
+    if(key === 13){
+        let gif = inputText.value;
+        console.log(gif)
+        inputText.value = "";
+
+        fetch(`http://api.giphy.com/v1/gifs/search?q=ryan+gosling${gif}&api_key=93ePOjRX0BY9dX5QhGDoC2J6XdoMasrR&limit=5`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            renderInfo(data);
+        })
+    }
+    
+})
+
+const renderInfo = data =>{
+   
+}
